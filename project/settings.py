7eca93a421dfa8b9a93b161299a14c4b42fd0f29
Navneet Singh <singhnavneet@singhna-ltmmyhr.internal.salesforce.com>
2023-07-06 13:50:43 +0530

    Some changes

--- project/settings.py
+++ project/settings.py
@@ -24,9 +24,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-9#-g@8m!r!bwe&9gki40p8e(792a!7)x_b80&ep9=-s4rcwqvo'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 # Application definition
 
