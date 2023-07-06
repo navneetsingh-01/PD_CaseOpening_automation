62ddf3b8797a4be3f23a545a354ecb4d9f631280
Navneet Singh <singhnavneet@singhna-ltmmyhr.internal.salesforce.com>
2023-07-06 15:08:28 +0530

    Some changes

--- project/models.py
+++ project/models.py
@@ -1,7 +1,7 @@
 from django.db import models
  
 class Event(models.Model):
-    name = models.CharField(max_length=200)
+    name = models.CharField(max_length=300)
 
     def __str__(self):
         return self.name 
--- project/settings.py
+++ project/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
 from pathlib import Path
+import dj_database_url
 
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -77,10 +78,16 @@ WSGI_APPLICATION = 'project.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'db_test', 
+        'USER': 'postgres',
+        'PASSWORD': 'postgres',
+        'HOST': '127.0.0.1', 
+        'PORT': '5432',
     }
 }
+db_from_env = dj_database_url.config(conn_max_age=600)
+DATABASES['default'].update(db_from_env)
 
 
 # Password validation
