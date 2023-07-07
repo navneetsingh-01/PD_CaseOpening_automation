eaa8d8ee969056eaaf96205a803049a8b9d2694e
Navneet Singh <singhnavneet@singhna-ltmmyhr.internal.salesforce.com>
2023-07-07 11:16:33 +0530

    Some changes

--- project/settings.py
+++ project/settings.py
@@ -12,7 +12,6 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 
 from pathlib import Path
 
-
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -24,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-9#-g@8m!r!bwe&9gki40p8e(792a!7)x_b80&ep9=-s4rcwqvo'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
  
 ALLOWED_HOSTS = ['*']
 
@@ -127,6 +126,3 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
-
-import django_on_heroku
-django_on_heroku.settings(locals())
